fadc19f82fa4d19777e2a5c3ce1620288782384a
JackAlneyra <j.alneyra0027@gmail.com>
2024-03-13 17:55:33 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre= "Marta";
+//nombre ="jose";
+//var nombre1= "jhon";
+
+
+//const nombre2= "Marta";
+//nombre2 = "jose";
+
+//console.log(nombre);
+//console.log(nombre1);
+//console.log(nombre2);
+
+//SELECCIONAR ELEMENTOS
+
+//let contenidoTitulo = "nombr";
+
+//let Titulo = document.querySelector(".logo .fuente-acento");
+//Titulo.innerHTML = contenidoTitulo;
+
+//CONDICIONALES
+
+//let textoTitulo =Titulo.innerText;
+//console.log(textoTitulo);
+
+//if (textoTitulo== "nombre") {
+  //  Titulo.innerHTML = "otro";
+//} else {
+  //  console.log("no se cumple");
+//}
+
+//var h2=Acerca de mí;
+
+//if (h3==Acerca de mí) {
+  //  console.log("acerca");
+    //else {
+      //  console.log("tengo que");
+    //}
+//}
+//FUNCIONES
+
+// let nombre = "jack";
+// let ciudad = "Perusalem";
+
+// let parrafo = document.querySelector (".parrafo2");
+
+// function cambiarTexto (nombre, ciudad) {
+//     let contenido = ´Lorem $(nombre) $(ciudad), sit amet consectetur adipisicing elit. 
+//     Similique, magni. Ad numquam quisquam omnis dolore quam hic aperiam tempore laudantium earum nam. 
+//     Aut impedit delectus cum laborum maxime, optio vitae.´;
+
+//     return contenido;
+// }
+
+// parrafo.innerText= cambiarTexto(nombre, ciudad);
